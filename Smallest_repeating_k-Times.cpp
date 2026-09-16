f64ea35dca54b18ffc62c2f484fa41599327a9b7
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-16 23:01:16 +0530

    Smallest element repeating k times

---
+++ Smallest_repeating_k-Times.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int findDuplicate(const vector<int>& arr, int k) 
+    {
+        // code here
+        int count = 0;
+        
+        unordered_map<int,int>um;
+        for(int i=0;i<arr.size();i++)
+        {
+            um[arr[i]]++;
+        }
+        
+        int min = INT_MAX;
+        for(auto x : um)
+        {
+            if(x.second == k)
+            {
+                if(min > x.first)
+                    min = x.first;
+            }
+        }
+        
+        if(min != INT_MAX)
+            return min;
+        else
+            return -1;
+    }
+};
