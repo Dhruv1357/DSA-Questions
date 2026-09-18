d707ca2ec8c9d2cdcf72c2e8568766c9fa277683
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-18 22:56:52 +0530

    Reversing the array

---
+++ Reverse_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) 
+    {
+        // code here
+        int temp;
+        
+        for(int i=0;i<arr.size()/2;i++)
+        {
+            temp = arr[i];
+            arr[i] = arr[arr.size() - i - 1];
+            arr[arr.size() - i - 1] = temp;
+        }
+    }
+};
