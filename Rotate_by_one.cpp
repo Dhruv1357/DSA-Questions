8949818eca56178fdb28f57b8734af86e24a962b
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-20 23:02:23 +0530

    Rotate the array by one

--- Rotate_by_one.cpp
+++ Rotate_by_one.cpp
@@ -1,11 +1,11 @@
-// User function Template for C++
+#include <iostream>
+using namespace std;
 
 class Solution {
   public:
     void rotate(vector<int> &arr) 
     {
-        // code here
-        
+
         int temp = arr[arr.size()-1];
         for(int i=arr.size()-1;i>0;i--)
         {
