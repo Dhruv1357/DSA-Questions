fd42174714d9be63e7d928d3aceec01b09d7f10b
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-09-22 23:00:30 +0530

    Rotate the array by one

--- Rotate_by_one.cpp
+++ Rotate_by_one.cpp
@@ -8,11 +8,8 @@ class Solution
     {
 
         int temp = arr[arr.size()-1];
-        
-        for(int i=arr.size()-1;i>0;i--)
-        {
-            arr[i] = arr[i-1];
-        }
+
+        for(int i=arr.size()-1;i>0;i--)arr[i] = arr[i-1];
 
         arr[0] = temp;
     }
